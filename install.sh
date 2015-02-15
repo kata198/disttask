393464cf00fa65c436efd5ae3db74b1c25c9441f
Tim Savannah <kata198@gmail.com>
2015-02-15 01:22:47 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,41 +1,122 @@
 #!/bin/bash
 
 
-APP_NAME="disttask"
+INSTALL_DIR=
+PROVIDED_PREFIX=
 
+print_usage() {
+	echo -en "install.sh: Installs disttask\n\n";
+	echo -en "Install Location:\n  Install location is determined by following means, in order:\n\n";
+	echo -e "    1. --install-dir=DIRECTORY             Installs the program into the given directory"
+	echo -e "    2. --prefix=PATH                       Installs program into \"PATH\"/bin. Ex: --prefix=/usr or --prefix=~/";
+	echo -e "    3. \$PREFIX environment variable        Installs the  program into \$PREFIX/bin. Ex: PREFIX=/usr/local ./install.sh";
+	echo -e "    4. If /usr/bin is writeable            Installs program into /usr/bin";
+	echo -e "    5. If \$HOME is writeable               Installs program into \$HOME/bin";
+	echo    "    6. Out of luck, bud.                   Prints this message.";
+}
+
+echoerr() {
+	echo $@ >&2
+}
+
+strip_trailing_slash() {
+	echo $@ | sed 's|/$||'
+}
+
+check_install_dir_overlap() {
+	if [ ! -z "$INSTALL_DIR" -o ! -z "$PROVIDED_PREFIX" ];
+	then
+		echoerr -e "May only use one of --prefix or --install-dir in arguments\n\n\n";
+		print_usage
+		exit 1;
+	fi
+	return 0;
+}
 
 for arg in "$@"
 do
 	if [ "$arg" = "--help" ] || [ "$arg" = "-h" ] || [ "$arg" = "-?" ];
 	then
-		echo -e "Usage: install.sh\n  Installs the '${APP_NAME}' application.\n\tYou can provide the --prefix=DIRECTORY (e.x. --prefix=/usr/local), or PREFIX env variable (used in that order), or default /usr/ to specify the install directory.\n\tYou can also provide --install-dir, or INSTALL_DIR env variable (preferred in that order) to specify the exact directory to place the binary. This overrides anything in 'prefix' directives.";
-		exit;
+		print_usage;
+		exit 0;
 	elif [ `expr match "$arg" "^--prefix="` -gt 0 ];
 	then
-		PREFIX="`echo $arg | sed 's/^--prefix=//'`"
+		check_install_dir_overlap;
+		PROVIDED_PREFIX="`echo $arg | sed 's/^--prefix=//'`"
 	elif [ `expr match "$arg" "^--install-dir="` -gt 0 ];
 	then
+		check_install_dir_overlap;
 		INSTALL_DIR="`echo $arg | sed 's/^--install-dir=//'`"
+	else
+		echoerr "WARNING: Unknown argument $arg\n"
 	fi
 done
 
-PACKAGE_DIRECTORY=`dirname ${BASH_SOURCE[0]}`;
-
-
-if [ -z "${INSTALL_DIR}" ];
+if [ -z "$INSTALL_DIR" ];
 then
-
-		if [ -z "${PREFIX}" ];
+	if [ -n "$PROVIDED_PREFIX" ];
+	then
+		PROVIDED_PREFIX="`strip_trailing_slash $PROVIDED_PREFIX`"
+		if [ ! -d "$PROVIDED_PREFIX" ];
 		then
-			PREFIX="/usr"
-		else
-			PREFIX="`echo $(bash -c \"cd ${PREFIX} && pwd\")`"
+			echoerr "No such file or directory: $PROVIDED_PREFIX"
+			exit 1;
 		fi
-		INSTALL_DIR="${PREFIX}/bin"
-else
-	INSTALL_DIR="`echo $(bash -c \"cd ${INSTALL_DIR} && pwd\")`"
+		INSTALL_DIR="$PROVIDED_PREFIX/bin"
+	elif [ -n "$PREFIX" ];
+	then
+		PREFIX="`strip_trailing_slash $PREFIX`"
+		if [ ! -d "$PREFIX" ];
+		then
+			echoerr "No such file or directory: $PREFIX"
+			exit 1;
+		fi
+		INSTALL_DIR="$PREFIX/bin"
+	elif [ -w "/usr/bin" ];
+	then
+		INSTALL_DIR='/usr/bin'
+	elif [ -n "$HOME" ] && [ -w "$HOME" -o -w "$HOME/bin" ];
+	then
+		INSTALL_DIR="`strip_trailing_slash $HOME`/bin"
+	else
+		echoerr "Cannot find any place to install. Please provide a directory:\n\n"
+		print_usage;
+		exit 1;
+	fi
 fi
 
-echo "Installing ${APP_NAME} to ${INSTALL_DIR}"
+if [ ! -d "$INSTALL_DIR" ];
+then
+	INSTALL_DIR_PARENT="`dirname $INSTALL_DIR`"
+	if [ -w "$INSTALL_DIR_PARENT" ];
+	then
+		echoerr "Warning, Install directory $INSTALL_DIR is not present, will create.";
+		mkdir -p $INSTALL_DIR
+		if [ $? -ne 0 ];
+		then
+			echoerr "Could not create install directory $INSTALL_DIR . Please check and try again.";
+			exit 1;
+		fi
+	else
+		echoerr "Could not install to directory $INSTALL_DIR . Check permissions and try again.";
+		exit 1;
+	fi
+fi
+if [ ! -w "$INSTALL_DIR" ];
+then
+	echoerr "Could not install to directory $INSTALL_DIR . Check permissions and try again.";
+	exit 1;
+fi
 
-install -m 755 ${PACKAGE_DIRECTORY}/${APP_NAME} ${INSTALL_DIR}
+
+PACKAGE_DIRECTORY=`dirname ${BASH_SOURCE[0]}`;
+
+echo -e "[I]\tdisttask\t\t--->\t$INSTALL_DIR/disttask"
+install -m 775 $PACKAGE_DIRECTORY/disttask $INSTALL_DIR/
+if [ $? -ne 0 ];
+then
+	echoerr 'Failed to install';
+	exit 1;
+fi
+
+# vim: ts=4 sw=4 noexpandtab
